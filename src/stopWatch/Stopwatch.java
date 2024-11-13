afa575f9d77095554a227e390bd15ce0e9a4ecd5
gusrbs-0404 <gusrbs7534@gmail.com>
2024-11-13 14:08:45 +0900

    Add menu(), stop(), hold(), rerun() method

--- src/stopWatch/Stopwatch.java
+++ src/stopWatch/Stopwatch.java
@@ -1,6 +1,12 @@
 package stopWatch;
 
+import java.util.Scanner;
+
 public class Stopwatch extends Watch {
+
+	private final String STOP = "q";
+	private final String HOLD = "w";
+	private final String RERUN = "e";
 
 	private void Stopwatch() {
 
@@ -12,18 +18,57 @@ public class Stopwatch extends Watch {
 		return instance;
 	}
 
+	private Scanner scan = new Scanner(System.in);
+
+	Watch watch = new Watch();
+	Thread watchThread = new Thread(watch);
+
+	@Override
 	public void run() {
-		while (true) {
-			second++;
-			printTime();
 
-			try {
-				Thread.sleep(1000);
+		watchThread.start();
 
-			} catch (InterruptedException e) {
-				e.printStackTrace();
-			}
+	}
 
+	private void menu() {
+		System.out.println("[q] STOP");
+		System.out.println("[w] HOLD");
+		System.out.println("[e] RERUN");
+
+		String select = input("메뉴 입력");
+
+		if (select.equals(STOP)) {
+			stop();
+		} else if (select.equals(HOLD)) {
+			hold();
+		} else if (select.equals(RERUN)) {
+			rerun();
 		}
 	}
+
+	private void stop() {
+		// TODO Auto-generated method stub
+
+	}
+
+	private void hold() {
+		// TODO Auto-generated method stub
+
+	}
+
+	private void rerun() {
+		// TODO Auto-generated method stub
+
+	}
+
+	private String input(String msg) {
+		while (true) {
+			System.out.print(msg + " : ");
+			String input = scan.nextLine();
+
+			if (!input.equals(""))
+				return input;
+		}
+	}
+
 }
