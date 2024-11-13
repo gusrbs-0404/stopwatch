c2f63f334af41d30a8f231cc47c22e90b6e9d4e6
gusrbs-0404 <gusrbs7534@gmail.com>
2024-11-13 12:07:50 +0900

    Add stopwatch class

---
+++ src/stopWatch/Stopwatch.java
@@ -0,0 +1,18 @@
+package stopWatch;
+
+public class Stopwatch {
+
+	private void Stopwatch() {
+
+	}
+
+	private static Stopwatch instance = new Stopwatch();
+
+	public static Stopwatch getInstance() {
+		return instance;
+	}
+
+	public void run() {
+
+	}
+}
